aa91335ea755381aa065eb9fe4e31b4cb6392853
ml-engineer <bbolsoj592@gmail.com>
2024-09-05 00:00:25 +0500

    add login logoot registrations

---
+++ projectUII/custom_auth/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from django.contrib import admin
+
+from .views import UserLoginView,UserCreateView
+from django.contrib.auth.views import LogoutView
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/',UserCreateView.as_view(), name='register'),
+]
